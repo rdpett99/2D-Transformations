9757e5e2f01feb24ede28dcca7211740bb5a140d
rdpett99 <rdp06865@uga.edu>
2023-02-27 15:31:55 -0500

    tested rotation matrix on triangle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import transformations as tf
+import numpy as np
 
+from shape import Shape
+import transformations as tf
 import basic_line_algorithm as basic
 
 def draw_coord_system():
@@ -14,7 +16,10 @@ def draw_coord_system():
 def draw_shape(shape: str):
     """
     Draws either a square or a triangle, determined by the
-    user.
+    user. 
+    
+    NOTE: The center of the square is (350, 100) and the center 
+    of the triangle is (330, 125)
     """
     if shape == 'square':
         basic.draw_line(300, 50, 300, 150)
@@ -29,5 +34,24 @@ def draw_shape(shape: str):
 
 if __name__ == '__main__':
     draw_coord_system()
-    draw_shape('triangle')
+    prompt = 'What shape would you like to draw?\n'
+    prompt += 'Type 1 for Square, or 2 for Triangle: '
+    answer = int(input(prompt))
+    if answer == 1:
+        shape = Shape('square')
+    elif answer == 2:
+        shape = Shape('triangle')
+    draw_shape(shape.shape_name)
+
+    vertices = shape.get_vertices()
+    rotation_matrix = tf.rotate(180, 330, 125)
+
+    p1 = [vertices[0][0], vertices[0][1], 1] @ rotation_matrix
+    p2 = [vertices[1][0], vertices[1][1], 1] @ rotation_matrix
+    p3 = [vertices[2][0], vertices[2][1], 1] @ rotation_matrix
+
+    basic.draw_line(int(p1[0]), int(p1[1]), int(p2[0]), int(p2[1]))
+    basic.draw_line(int(p2[0]), int(p2[1]), int(p3[0]), int(p3[1]))
+    basic.draw_line(int(p3[0]), int(p3[1]), int(p1[0]), int(p1[1]))
+
     basic.image.show()
