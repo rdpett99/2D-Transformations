49c1b8b02a95f346e558c79d7b6e44bec6c03738
rdpett99 <rdp06865@uga.edu>
2023-02-28 09:05:55 -0500

    finished shape.py

--- shape.py
+++ shape.py
@@ -35,3 +35,29 @@ class Shape:
         elif self.shape_name == 'triangle':
             self.vertices = [(points[0], points[1]), (points[2], points[3]),
                              (points[4], points[5])]
+    
+    def get_center(self):
+        """
+        Returns a shape's center in the form of a tuple.
+        """
+        if self.shape_name == 'square':
+            x = int(sum([v[0] for v in self.vertices]) / 4)
+            y = int(sum([v[1] for v in self.vertices]) / 4)
+        elif self.shape_name == 'triangle':
+            x = int(sum([v[0] for v in self.vertices]) / 3)
+            y = int((sum([v[1] for v in self.vertices]) - self.get_max_y()) / 2)
+        if x < 0:
+            x += 500
+        if y < 0:
+            y += 500
+        return (x, y)
+
+    def get_max_y(self):
+        """
+        Helper method.
+        """
+        max_y = None
+        for x, y in self.vertices:
+            if max_y is None or y > max_y:
+                max_y = y
+        return max_y
