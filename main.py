ea7eb6a70a905f0f6c51a1c9f776d7aee395f5a8
rdpett99 <rdp06865@uga.edu>
2023-02-28 16:22:40 -0500

    implemented line transformations

--- main.py
+++ main.py
@@ -44,18 +44,24 @@ def reset():
 def prompt_user():
     """
     Prompts the user for input.
-
-    *** NOTE: NEED TO UPDATE FOR LINE TRANSFORMATIONS ***
     """
     prompt = '\nWould you like to draw a singular line or a shape?\n'
     prompt += 'Type 1 for drawing your own line, or 2 for a pre-drawn shape: '
     answer = int(input(prompt))
+
+    # Line
     if answer == 1:
         x0 = int(input('Enter coordinate for x0: '))
         y0 = int(input('Enter coordinate for y0: '))
         x1 = int(input('Enter coordinate for x1: '))
         y1 = int(input('Enter coordinate for y1: '))
+
+        # Shows the line and prompts transformation
         basic.draw_line(x0, y0, x1, y1)
+        basic.image.show()
+        apply_line_tf(x0, y0, x1, y1)
+
+    # Shape
     elif answer == 2:
         prompt = '\nWhat shape would you like to draw?\n'
         prompt += 'Type 1 for Square, or 2 for Triangle: '
@@ -64,15 +70,85 @@ def prompt_user():
             shape = Shape('square')
         elif answer == 2:
             shape = Shape('triangle')
-        draw_shape(shape)
 
-    # Shows the starting image.
+        # Shows the starting image and applies the transformation
+        draw_shape(shape)
+        basic.image.show()
+        apply_shape_tf(shape)
+
+def apply_line_tf(x0, y0, x1, y1):
+    """
+    This function prompts the user for information regarding
+    which transformation the user wishes to perform. The function
+    then calculates the new vertices, draws the new line onto the
+    screen.
+    """
+    prompt = '\nWhich transformation would you like to perform?\n'
+    prompt += 'Type 1 for Translate, 2 for Scale, or 3 for Rotate: '
+    answer = int(input(prompt))
+
+    # Placeholder for final matrix
+    matrix = tf.basic_translate(0, 0)
+
+    # Translation
+    if answer == 1:
+        Tx = int(input('\nEnter x-displacement: '))
+        Ty = int(input('Enter y-displacement: '))
+        matrix = tf.basic_translate(Tx, Ty)
+
+    # Basic Scale
+    elif answer == 2:
+        Sx = float(input('\nEnter x scaling factor: '))
+        Sy = float(input('Enter y scaling factor: '))
+        Cx, Cy = 0, 0
+        matrix = tf.basic_scale(Sx, Sy)
+        prompt = '\nWould you like to perform a basic scale (centered at the origin)\n'
+        prompt += 'or a general scale (around specified point)?\n'
+        prompt += 'Type 1 for basic scale, or 2 for general scale: '
+        choice = int(input(prompt))
+
+        # General Scale
+        if choice == 2:
+            print(f'\nNOTE: Center of line is ({int((x0 + x1) / 2)}, {int((y0 + y1) /2)})\n')
+            Cx = int(input('Enter x-coordinate for center of scale: '))
+            Cy = int(input('Enter y-coordinate for center of scale: '))
+            matrix = tf.scale(Sx, Sy, Cx, Cy)
+            
+    # Basic Rotation
+    elif answer == 3:
+        angle = int(input('\nHow many degrees clockwise do you wish to rotate the shape? '))
+        Cx, Cy = 0, 0
+        matrix = tf.basic_rotate(angle)
+        prompt = '\nWould you like to perform a basic rotation (around the origin)\n'
+        prompt += 'or a general rotation (around specified point)?\n'
+        prompt += 'Type 1 for basic rotation, or 2 for general rotation: '
+        choice = int(input(prompt))
+
+        # General Rotation
+        if choice == 2:
+            print(f'\nNOTE: Center of line is ({int((x0 + x1) / 2)}, {int((y0 + y1) /2)})\n')
+            Cx = int(input('Enter x-coordinate for center of rotation: '))
+            Cy = int(input('Enter y-coordinate for center of rotation: '))
+            matrix = tf.rotate(angle, Cx, Cy)
+    
+    # Calculates new vertices
+    p1 = [x0, y0, 1] @ matrix
+    p2 = [x1, y1, 1] @ matrix
+
+    reset()
+
+    # Draws new line
+    basic.draw_line(int(p1[0]), int(p1[1]), int(p2[0]), int(p2[1]))
+
     basic.image.show()
 
-    # Applies transformation.
-    apply_tf(shape)
+    prompt = '\nWould you like to perform another transformation?\n'
+    prompt += 'Type 1 for yes, or 2 for no: '
+    ans = int(input(prompt))
+    if ans == 1:
+        apply_line_tf(int(p1[0]), int(p1[1]), int(p2[0]), int(p2[1]))
 
-def apply_tf(shape):
+def apply_shape_tf(shape):
     """
     This function prompts the user for information regarding
     which transformation the user wishes to perform. The function
